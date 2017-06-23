f3ef9eac8cbb63c4a146a411033fce7b5fb89e7a
6UBeHb <bykov89ilya@gmail.com>
2017-06-24 02:50:04 +0300

    Add elements

---
+++ LinkedinBot/Configuration/KeyWordElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace LinkedinBot.Configuration
+{
+    public class KeyWordElement : ConfigurationElement
+    {
+        [ConfigurationProperty("word")]
+        public string Word
+        {
+            get
+            {
+                return (string)this["word"];
+            }
+        }
+    }
+}
---
+++ LinkedinBot/Configuration/MakeContactElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace LinkedinBot.Configuration
+{
+    public class MakeContactElement : ConfigurationElement
+    {
+        [ConfigurationProperty("text")]
+        public string Text
+        {
+            get
+            {
+                return (string)this["text"];
+            }
+        }
+    }
+}
