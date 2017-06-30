de088699bff598029f200c17c50386914951b9e9
6UBeHb <bykov89ilya@gmail.com>
2017-06-30 10:33:31 +0300

    Add logging, making screenshots

---
+++ LinkedinBot/ScreenshotHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LinkedinBot
+{
+    public static class ScreenshotHelper
+    {
+        private static DirectoryInfo directoryForScreenshots = new DirectoryInfo(
+            Path.Combine(Directory.GetCurrentDirectory(), "Screenshots"));
+
+        public static void Make(int id, string postfix)
+        {
+            Graphics graph = null;
+
+            var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            graph = Graphics.FromImage(bmp);
+
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+
+            bmp.Save(Path.Combine(directoryForScreenshots.ToString(), 
+                string.Format("{0}_{1}_{2}.png", GetCorretDateTime(), id, postfix)));
+        }
+
+        private static string GetCorretDateTime()
+        {
+            return DateTime.Now.ToString("yy_MM_dd_h_m_s");
+        }
+    }
+}
--- LinkedinBot/WebTraveler.cs
+++ LinkedinBot/WebTraveler.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Threading;
 using System.Web;
 using LinkedinBot.Configuration;
+using NLog;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -13,6 +14,7 @@ namespace LinkedinBot
     {
         public IWebDriver Driver;
         private readonly JobVacanciesConfigSection _jobVacanciesConfigSection;
+        private Logger logger = LogManager.GetCurrentClassLogger();
 
         public WebTraveler()
         {
@@ -45,16 +47,33 @@ namespace LinkedinBot
 
         private void ScrollDown()
         {
-            //var elementToScroll = Driver.FindElement(By.XPath("//*[contains(text(),'LinkedIn Corporation')]"));
             var script = String.Format("window.scrollTo({0}, {1})", 0, 12000);
             ((IJavaScriptExecutor)Driver).ExecuteScript(script);
             Thread.Sleep(TimeSpan.FromSeconds(10));
         }
 
+        private void ScrollDownBySteps()
+        {
+            for (int i = 1000; i <= 4000; i = i + 100)
+            {
+                var script = String.Format("window.scrollTo({0}, {1})", 0, i);
+                ((IJavaScriptExecutor)Driver).ExecuteScript(script);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+        }
+
         private void ScrollUp()
         {
-            //var elementToScroll = Driver.FindElement(By.XPath("//*[contains(text(),'LinkedIn Corporation')]"));
             var script = String.Format("window.scrollTo({0}, {1})", 0, 0);
+            ((IJavaScriptExecutor)Driver).ExecuteScript(script);
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+        }
+
+        private void ScrollToCoordinates(int x, int y)
+        {
+            // on the middle of window
+            y = y - 300;
+            var script = String.Format("window.scrollTo({0}, {1})", x, y);
             ((IJavaScriptExecutor)Driver).ExecuteScript(script);
             Thread.Sleep(TimeSpan.FromSeconds(10));
         }
@@ -66,29 +85,49 @@ namespace LinkedinBot
             // Enumerate regions
             foreach (RegionElement region in _jobVacanciesConfigSection.RegionsItems)
             {
+                logger.Trace("Исследуем регион: {0}", region.Place);
+
                 // Enumerate vacancies
                 foreach (VacancyElement vacancy in _jobVacanciesConfigSection.VacanciesItems)
                 {
+                    logger.Trace("Ищем людей на вакансию {0} c id {1}", vacancy.Description, vacancy.Id);
+
                     // Enumerate pages
                     for (int page = 1; page <= 100; page++)
                     {
-                        Driver.Navigate().GoToUrl(
-                        string.Format(pattern, 
-                            region.Place, HttpUtility.UrlEncode(vacancy.SearchWord), page));
+                        var urlToAnalyse = string.Format(pattern,
+                            region.Place, HttpUtility.UrlEncode(vacancy.SearchWord), page);
+
+                        Driver.Navigate().GoToUrl(urlToAnalyse);
+
+                        logger.Trace("Анализируем страницу {0}, переходя по url {1}", page, urlToAnalyse);
 
                         ScrollDown();
 
                         var accountLinks = Driver.FindElements(By.XPath("//div[contains(@class,'search-result__info')]/a"));
+
+                        logger.Trace("Количество аккаунтов на странице {0}", accountLinks.Count);
 
                         if (accountLinks.Count == 0) break;
 
                         // Enumerate accounts on page
                         foreach (var link in accountLinks)
                         {
+                            logger.Trace("Анализируем ссылку на аккаунт {0}", link.Text);
+
                             try
                             {
                                 var buttonMakeContact = link.FindElement(By.XPath("../..")).FindElement(By.ClassName("search-result__actions--primary"));
-                                if (!buttonMakeContact.Text.Contains("Установить контакт")) continue;
+                                if (!buttonMakeContact.Text.Contains("Установить контакт"))
+                                {
+                                    logger.Trace(
+                                        "Справа от ссылки на аккаунт нет кнопки \"Установить контакт\", текст на кнопке \"{0}\"", buttonMakeContact.Text);
+                                    continue;
+                                }
+                                else
+                                {
+                                    logger.Trace("Анализируем страницу по ссылке на аккаунт {0}", link.Text);
+                                }
                             }
                             catch(NotFoundException)
                             {
@@ -102,60 +141,95 @@ namespace LinkedinBot
                             Driver.SwitchTo().Window(newWindowHandle);
                             Thread.Sleep(TimeSpan.FromSeconds(30));
 
-                            ScrollDown();
+                            ScrollDownBySteps();
 
                             var expandSkillsButton = Driver.FindElement(By.XPath("//*[contains(@class,'pv-skills-section__additional-skills')]"));
+                            ScrollToCoordinates(
+                                x: expandSkillsButton.Location.X,
+                                y: expandSkillsButton.Location.Y);
                             expandSkillsButton.Click();
 
                             var skills = Driver.FindElements(By.ClassName("pv-skill-entity__skill-name"));
                             var textSkills = new List<string>();
                             foreach (var skill in skills)
                             {
+                                logger.Trace("Найден скилл {0}", skill.Text);
                                 textSkills.Add(skill.Text); 
                             }
                             var existKeyWordsOnPage = true;
-                            //foreach (KeyWordElement keyWord in vacancy.keyWords)
-                            //{
-                            //    var words = keyWord.Word.Split(new[] { '|' });
-                            //    foreach (var word in words)
-                            //    {
-                            //        if (textSkills.Contains(word))
-                            //        {
-                            //            existKeyWordsOnPage = true;
-                            //            break;
-                            //        }
-                            //        else
-                            //        {
-                            //            existKeyWordsOnPage = false;
-                            //        }
-                            //    }
-                            //    if (!existKeyWordsOnPage) break;
-                            //}
+                            foreach (KeyWordElement keyWord in vacancy.keyWords)
+                            {
+                                logger.Trace("Ищем слово(а) в скиллах {0}", keyWord.Word);
+                                var words = keyWord.Word.Split(new[] { '|' });
+                                foreach (var word in words)
+                                {
+                                    if (textSkills.Contains(word))
+                                    {
+                                        existKeyWordsOnPage = true;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        existKeyWordsOnPage = false;
+                                    }
+                                }
+                                if (!existKeyWordsOnPage) break;
+                            }
                             // If true make contact
                             if (existKeyWordsOnPage)
                             {
                                 try
                                 {
+                                    logger.Trace("Пытаемся установить контакт");
+
                                     ScrollUp();
 
-                                    var buttonMakeContact = Driver.FindElement(By.XPath(string.Format("//*[contains(text(), '{0}')]/..", "Установить контакт")));
-                                    buttonMakeContact.Click(); 
+                                    logger.Trace("Ищем кнопку \"Установить контакт\"");
 
-                                    var buttonPersonalize = Driver.FindElement(By.XPath("//*[.='Персонализировать']"));
+                                    var buttonMakeContact = Driver.FindElement(By.XPath(string.Format("//*[contains(text(), '{0}')]/..", "Установить контакт")));
+                                    buttonMakeContact.Click();
+
+                                    Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                                    ScreenshotHelper.Make(vacancy.Id, "makeContact");
+
+                                    logger.Trace("Ищем кнопку \"Персонализировать\"");
+
+                                    var buttonPersonalize = Driver.FindElement(By.XPath("//*[contains(@class,'send-invite__actions')]/button[contains(@class,'button-secondary-large')]"));
                                     buttonPersonalize.Click();
+
+                                    Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                                    ScreenshotHelper.Make(vacancy.Id, "Personalize");
+
+                                    logger.Trace("Ищем поле для ввода текста приглашения");
 
                                     var textAreaMessage = Driver.FindElement(By.Id("custom-message"));
                                     textAreaMessage.SendKeys(_jobVacanciesConfigSection.MakeContact.Text + " " + vacancy.Id);
 
-                                    var buttonInvite = Driver.FindElement(By.XPath(string.Format("//*[contains(text(), '{0}')/..]", "Отправить приглашение")));
+                                    ScreenshotHelper.Make(vacancy.Id, "InputInTextArea");
+
+                                    logger.Trace("Ищем кнопку \"Отправить\"");
+
+                                    var buttonInvite = Driver.FindElement(By.XPath("//*[contains(@class,'send-invite__actions')]/button[contains(@class,'ml3')]"));
                                     buttonInvite.Click();
                                     Thread.Sleep(TimeSpan.FromSeconds(30));
+
+                                    ScreenshotHelper.Make(vacancy.Id, "SentInvite");
+
+                                    logger.Trace("Контакт установлен");
                                 }
-                                catch(NotFoundException)
+                                catch (NotFoundException ex)
                                 {
-                                    
+                                    logger.Error("Возникла ошибка при установлении контакта: {0}",
+                                        ex.Message);
                                 }
                             }
+                            else
+                            {
+                                logger.Trace("Страница не содержит нужные скиллы для вакансии");
+                            }
+                            logger.Trace("Закрываем вкладку, возращаемся к основному списку");
                             Driver.Close();
                             Driver.SwitchTo().Window(mainWindowHandle);
                         }
